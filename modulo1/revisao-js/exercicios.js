364a43289371af1a15603e3643596ac38244bed4
Julia Santos de Oliveira <Julia@MacBook-de-Julia.local>
2021-10-18 22:41:55 -0300

    revisao js

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -15,27 +15,63 @@ function retornaArrayInvertido(array) {
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  
+   return array.sort(comparaNumeros)
 }
+
+function comparaNumeros(a,b) { if (a == b) return 0; if (a < b) return -1; if (a > b) return 1; }
+
+
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  
+  let  numerosPares = array.filter((item, indice, array) => { 
+      return  item % 2 === 0
+  })
+      return numerosPares
 }
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
- 
+   let  numerosPares = array.filter((item, indice, array) => { 
+      return  item % 2 === 0
+  })
+  let numeroMultiPorSiMesmo = numerosPares.map(( item, indice, array) => {
+     return item * item
+  })
+  return numeroMultiPorSiMesmo
 }
 
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  
+  let maiorNumero = array[0]
+   for(let i = 0; i < array.length; i++){
+      if(maiorNumero < array[i]){
+         maiorNumero = array[i]
+      }
+   } 
+
+  return maiorNumero
 }
 
 // EXERCÍCIO 07
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+   let maiorNumero 
+   let diferenca
+   let validacao
+   if(num1 < num2){
+      maiorNumero = num2
+      diferenca = num2 - num1
+   }else{
+      maiorNumero = num1
+      diferenca = num1 - num2
+   }
 
+   console.log(num1,num2,maiorNumero,diferenca)
+   return {
+      maiorNumero: maiorNumero,
+      maiorDivisivelPorMenor:true,
+      diferenca:diferenca
+   }
 }
 
 // EXERCÍCIO 08
