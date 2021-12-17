f28231937533002611cd4744f57f96b66cfec8e6
Julia Oliveira <jucs.santos.oliveira@gmail.com>
2021-12-17 16:59:59 -0300

    cookenu

--- modulo3/projeto-cookenu/cookenu/src/App.js
+++ modulo3/projeto-cookenu/cookenu/src/App.js
@@ -1,9 +1,21 @@
-import react from 'react';
+import React, {useState} from "react"
+import Router from "./routes/routes/Router"
+import theme from './constants/constants/theme'
+import { BrowserRouter } from 'react-router-dom'
+import Header from "./components/headres/Headers"
 
-const  App = () => {
+const App = () => {
+  const token = localStorage.getItem("token")
+  const [rightButtonText, setRightButtonText] = useState(token ? "Logout" : "Login")
+
   return (
-    <div>Opa</div>
-  );
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header rightButtonText={rightButtonText} setRightButtonText={setRightButtonText} />
+        <Router setRightButtonText={setRightButtonText}/>
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
