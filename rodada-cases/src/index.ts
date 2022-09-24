289a32c56766212800c5dcdbf19144fb731543db
Julia Oliveira <juliaoliveira@MacBook-de-Julia.local>
2022-09-24 05:31:22 -0300

    consumindo e integrado com banco de dados

--- rodada-cases/src/index.ts
+++ rodada-cases/src/index.ts
@@ -3,37 +3,146 @@ import cors from 'cors'
 import { AddressInfo } from "net";
 import connection from "./connection";
 
-type competicao = {
-  id: number;
-  competicao: string;
-  status: string;
-  resultados:Array<resultados>;
-};
 
-type resultados = {
-  id:number;
-  atleta: string;
-  valor: number;
-  unidade: string;
-  posicao:any;
-}
-
-
-let competicoes: Array<competicao> = [];
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 const cadastrarCompeticao = async (
-  nome: string,
+  competicao: string,
 ): Promise<void> => {
   await connection.raw(`
         INSERT INTO competicoes
-          (competicao)
+          (competicao, status)
         VALUES (
-        "${nome}"
+        "${competicao}",
+        "não iniciada"
        );
+    `);
+};
+
+const iniciarCompeticao = async (
+  competicao: string,
+): Promise<void> => {
+  await connection.raw(`
+        Update 
+          competicoes
+        set 
+          status = "aberta"
+        where
+          competicao = "${competicao}" and status = "não iniciada";
+    `);
+};
+
+const fecharCompeticao = async (
+  competicao: string,
+): Promise<void> => {
+  await connection.raw(`
+        Update 
+          competicoes
+        set 
+          status = "fechada"
+        where
+          competicao = "${competicao}" and status = "aberta";
+    `);
+};
+
+const consultaCompeticaoAberta = async ( competicao: string): Promise<any> => {
+  const resultado = await connection("competicoes").column(["competicao"]).where({"competicao":competicao, "status":"aberta"});
+  //console.log(resultado[0]);
+  return resultado[0];
+};
+const consultaCompeticoes = async (): Promise<any> => {
+  const resultado = await connection("competicoes").column(["id","competicao","status"]);
+  //console.log(resultado[0]);
+  return resultado[0];
+};
+
+const consultaCompeticao = async (competicao: string): Promise<any> => {
+  const resultado = await connection.select("atleta","valor", "unidade").from('resultados')
+  .innerJoin('competicoes_resultados', 'resultados.id', 'competicoes_resultados.fk_id_resultado')
+  .innerJoin('competicoes','competicoes_resultados.fk_id_competicao','competicoes.id')
+  .where('competicoes.competicao', '=', competicao)
+  return resultado;
+};
+
+const consultaClassificaoCompeticaoAsc = async (data: any): Promise<any> => {
+  const resultado = await connection.raw(`
+  select 
+  RANK() OVER (
+       ORDER BY r.valor
+   ) posicao,
+     r.atleta,
+     CAST(r.valor AS DECIMAL(10,2)) as valor,
+     r.unidade
+   from 
+     resultados as r
+   inner join 
+     competicoes_resultados 
+   on
+     r.id = competicoes_resultados.fk_id_resultado
+   inner join
+     competicoes as c
+   on
+     c.id = competicoes_resultados.fk_id_competicao
+   where c.competicao = "${data.competicao}"
+   order by CAST(valor AS DECIMAL(10,3)) ASC ;
+    `)
+  
+  return resultado;
+};
+
+const consultaClassificaoCompeticaoDesc = async (data: any): Promise<any> => {
+  const resultado = await connection.raw(`
+  select 
+  RANK() OVER (
+       ORDER BY r.valor
+   ) posicao,
+     r.atleta,
+     CAST(r.valor AS DECIMAL(10,2)) as valor,
+     r.unidade
+   from 
+     resultados as r
+   inner join 
+     competicoes_resultados 
+   on
+     r.id = competicoes_resultados.fk_id_resultado
+   inner join
+     competicoes as c
+   on
+     c.id = competicoes_resultados.fk_id_competicao
+   where c.competicao = "${data.competicao}"
+   order by CAST(valor AS DECIMAL(10,3)) ASC ;
+    `)
+  
+  return resultado;
+};
+
+const cadastrarResultado = async (
+  data: any
+): Promise<void> => {
+  await connection.raw(`
+
+        INSERT INTO resultados
+          ( atleta,
+            valor,
+            unidade)
+        VALUES (
+        "${data.atleta}",
+        "${data.value}",
+        "${data.unidade}"
+       );
+       
+       INSERT INTO competicoes_resultados
+          (
+            fk_id_competicao,
+            fk_id_resultado
+          )
+          VALUES(
+            (select id from competicoes where competicao = "${data.competicao}"),
+            (select LAST_INSERT_ID())
+          );
     `);
 };
 
@@ -48,7 +157,7 @@ app.post("/cadastrarCompeticao",  async (req:Request,res:Response): Promise<void
     }
 
    await cadastrarCompeticao(
-     req.body.nome,
+     req.body.competicao,
    );
 
    res.status(201).send("Competicao cadastrado!");
@@ -57,7 +166,26 @@ app.post("/cadastrarCompeticao",  async (req:Request,res:Response): Promise<void
   }
 });
 
-app.put("/iniciarCompeticao", (req: Request, res: Response) => {
+app.put("/iniciarCompeticao", async (req: Request, res: Response): Promise<void> => {
+  let errorCode: number = 400;
+  let data = req.body?.competicao;
+  try {
+
+    if (!data) {
+      errorCode = 422;
+      throw new Error("Por favor preencha o campo vazio!");
+    }
+   
+    await iniciarCompeticao(data)
+
+    res.status(200).send({ message: `Competição ${data} Aberta.` });
+    
+  } catch (error: any) {
+    res.status(errorCode).send(error.sqlMessage || error.message);
+  }
+});
+
+app.put("/fecharCompeticao", async (req: Request, res: Response): Promise<void> => {
   let errorCode: number = 400;
   let data = req.body?.competicao;
 
@@ -68,75 +196,50 @@ app.put("/iniciarCompeticao", (req: Request, res: Response) => {
       throw new Error("Por favor preencha o campo vazio!");
     }
 
-    competicoes.map((item: any) =>{
-      if (item.competicao === data) {
-        item.status = "Aberto";
-      }
-    })
+    await fecharCompeticao(data)
 
-    res.status(200).send({ message: "Competição Aberta." });
+    res.status(200).send({ message: `Competição ${data} Fechada.`});
     
   } catch (error: any) {
     res.status(errorCode).send({ messagem: error.message });
   }
 });
 
-app.put("/fecharCompeticao", (req: Request, res: Response) => {
-  let errorCode: number = 400;
-  let data = req.body?.competicao;
-
-  try {
-
-    if (!data) {
-      errorCode = 422;
-      throw new Error("Por favor preencha o campo vazio!");
-    }
-
-    competicoes.map((item: any) =>{
-      if (item.competicao === data  && item.status === "Aberto") {
-        item.status = "Fechado";
-      }
-    })
-
-    res.status(200).send({ message: "Competição Fechada." });
-    
-  } catch (error: any) {
-    res.status(errorCode).send({ messagem: error.message });
-  }
-});
-
-app.put("/cadastrarResultado", (req: Request, res: Response) => {
+app.put("/cadastrarResultado", async(req: Request, res: Response): Promise<void> => {
   let errorCode: number = 400;
   let data = req.body;
 
   try {
 
-    if (!data.competicao) {
+    if (!data.competicao || !data?.atleta || !data?.value  || !data?.unidade) {
       errorCode = 422;
       throw new Error("Por favor preencha o campo vazio!");
     }
-
-    competicoes.map((item: any) =>{
-      if (item.competicao === data.competicao && item.status === "Aberto") {
-        item.resultados.push({
-          id: item.resultados.length + 1,
-          atleta: data.atleta,
-          valor: data.valor,
-          unidade: data.unidade
-        })
-      }
-    })
-
-    res.status(200).send({ message: "Resultado de competição atualizado." });
+    const competicoesAbertas = await consultaCompeticaoAberta(data.competicao);
+    
+    //await cadastrarResultado(data);
+    //console.log(competicoesAbertas?.competicao);
+    if(competicoesAbertas?.competicao !== undefined){
+      await cadastrarResultado(
+        data
+      );
+      res.status(200).send({ message: "Resultado de competição atualizado." });
+    }else{
+      res.status(404).send({ message: "Nenhuma competicão aberta com esse nome ou cadastrada." });
+    }
+    
     
   } catch (error: any) {
     res.status(errorCode).send({ messagem: error.message });
   }
 });
 
-app.get("/listaCompeticoes", (req: Request, res: Response)=>{
+app.get("/listaCompeticoes", async (req: Request, res: Response):Promise<void>=>{
   let errorCode: number = 400;
   try {
+
+    const competicoes = await consultaCompeticoes();
+    console.log(competicoes)
     res.status(200).send(competicoes);
   } catch (error: any) {
   
@@ -144,7 +247,7 @@ app.get("/listaCompeticoes", (req: Request, res: Response)=>{
   }
 });
 
-app.get("/listaCompeticao", (req: Request, res: Response)=>{
+app.get("/listaCompeticao", async(req: Request, res: Response):Promise<void>=>{
   let errorCode: number = 400;
   try {
     let data: any = req.body?.competicao;
@@ -152,18 +255,13 @@ app.get("/listaCompeticao", (req: Request, res: Response)=>{
       errorCode = 422;
       throw new Error("Por favor preencha o campo vazio!");
     }
-    let listCompeticao: any;
-
-    competicoes.map((item:any) =>{
-      if(item.competicao === data){
-        listCompeticao = item.resultados;
-      }
-  })
-
-  if(listCompeticao.length === 0 ){
-    res.status(200).send("Competições vazias");
+  
+    const competicao =  await consultaCompeticao(data)
+    console.log(competicao.length);
+  if(competicao.length === 0 ){
+    res.status(200).send("Competicao vazia");
   }else{
-    res.status(200).send(listCompeticao);
+    res.status(200).send(competicao);
   }
 
   } catch (error: any) {
@@ -172,7 +270,7 @@ app.get("/listaCompeticao", (req: Request, res: Response)=>{
   }
 });
 
-app.get("/listaRanqueadaCompeticao", (req: Request, res: Response)=>{
+app.get("/listaRanqueadaCompeticao", async(req: Request, res: Response): Promise<void>=>{
   let errorCode: number = 400;
   try {
     let data: any = req.body;
@@ -180,28 +278,18 @@ app.get("/listaRanqueadaCompeticao", (req: Request, res: Response)=>{
       errorCode = 422;
       throw new Error("Por favor preencha os campos vazios!");
     }
-    let listCompeticao: any;
+    let competicao : any;
 
-    competicoes.map((item:any) =>{
-      if(item.competicao === data.competicao){
-        listCompeticao = item.resultados;
+    
         if(data.ordenacao === "Crescente"){
-          listCompeticao.sort((a:any,b:any) => a.valor - b.valor);
-        }else{
-          listCompeticao.sort((a:any,b:any) => b.valor - a.valor);
-        }
+           competicao =  await consultaClassificaoCompeticaoAsc(data)
+         }else{
+          competicao =  await consultaClassificaoCompeticaoDesc(data)         }
+      if(competicao.length === 0 ){
+        res.status(200).send("Competicao vazia");
+      }else{
+        res.status(200).send(competicao);
       }
-    });
-
-    listCompeticao.forEach(function(itm:any){
-      itm.posicao = listCompeticao.findIndex((x:any) => x.id === itm.id) + 1;
-     });
-
-    if(listCompeticao.length === 0 ){
-        res.status(200).send("Competição vazia");
-    }else{
-        res.status(200).send(listCompeticao);
-    }
 
   } catch (error: any) {
   
